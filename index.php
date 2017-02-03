a1009224e52be5fd77a04ef5671b3e7330af74aa
Hesar Nur Ramaduloh <hesarnur@gmail.com>
2017-02-03 14:59:10 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,9 @@
+<?php
+Echo ("Nama  : Hesar Nur Ramaduloh ");
+Echo "<br>";
+Echo ("Kelas : 14  TIF Reguler Malam ");
+Echo "<br>";
+Echo ("NPM   : 14111218 ");
+Echo "<br>";
+Echo ("<<< SEKOLAH TINGGI TEKNOLOGI BANDUNG >>>");
+?>
